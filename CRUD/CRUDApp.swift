8683fda4c0f61d5c6a151961786499eb54ec953f
Mariangel Loaiza <loaurbmariangel@gmail.com>
2025-10-22 15:30:29 -0600

    Initial Commit

---
+++ CRUD/CRUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CRUDApp.swift
+//  CRUD
+//
+//  Created by Alumno on 22/10/25.
+//
+
+import SwiftUI
+
+@main
+struct CRUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
