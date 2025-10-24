30d698fdc2094d11c4419757f5e157c4ccdfe7d8
Mariangel Loaiza <loaurbmariangel@gmail.com>
2025-10-24 14:16:45 -0600

    rework: fixed bugs and cleaned code

---
+++ CRUD/Views/TaskDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskDetailsView.swift
+//  CRUD
+//
+//  Created by Alumno on 24/10/25.
+//
+
+import SwiftUI
+
+struct TaskDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskDetailsView()
+}
