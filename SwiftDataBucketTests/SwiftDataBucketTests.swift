bc5d2bb0a9f685d373e4a656f1a81c4bbe2078e0
Mariangel Loaiza <loaurbmariangel@gmail.com>
2025-10-24 12:34:27 -0600

    Testing

---
+++ SwiftDataBucketTests/SwiftDataBucketTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftDataBucketTests.swift
+//  SwiftDataBucketTests
+//
+//  Created by Alumno on 24/10/25.
+//
+
+import Testing
+
+struct SwiftDataBucketTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
